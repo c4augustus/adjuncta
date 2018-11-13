36277f5ec0886000510e3ec8cbea5827a66c734f
Christopher Augustus <repos@c4augustus.com>
2018-11-13 19:41:56 +0100

    fix setup-win10-stage6-run-ansible.sh to copy to, execute in, and then remove a temporary copy of adjuncta/setup/win

--- setup/win/setup-win10-stage6-run-ansible.sh
+++ setup/win/setup-win10-stage6-run-ansible.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cp -av . ../../../../temp-adjuncta-setup-win
-cd ../../../../temp-adjuncta-setup-win/ansible
-ansible-playbook -i ansible-inventory -vv ansible/ansible-main.yml
+cp -av . ../../../temp-adjuncta-setup-win
+cd ../../../temp-adjuncta-setup-win/ansible
+ansible-playbook -i ansible-inventory -vv ansible-main.yml
+cd ../../adjuncta/setup/win
+rm -fR ../../../temp-adjuncta-setup-win
