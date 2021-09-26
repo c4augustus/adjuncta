6c523c5b5b696604a38f5ed4503c58a763063e13
root <root@livecd.lan>
2021-09-26 20:41:38 +0000

    WIP completed stage3

--- bin/python/setup-gentoo.py
+++ bin/python/setup-gentoo.py
@@ -27,11 +27,11 @@ import subprocess
 import sys
 
 def abort(reason):
-    eprint("SETUP ABORTED: " + reason)
+    eprint('SETUP ABORTED: ' + reason)
     sys.exit(-1)
 
 def cancel():
-    abort("canceled")
+    abort('canceled')
 
 # from https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
 def eprint(*args, **kwargs):
@@ -40,7 +40,7 @@ def eprint(*args, **kwargs):
 # based on https://docs.python.org/2/tutorial/controlflow.html#defining-functions
 def ask_ok(prompt, retries=4, complaint='type y/ye/yes or n/no/nop/nope'):
     while True:
-        ok = input(prompt + "? (y/n): ")
+        ok = input(prompt + '? (y/n): ')
         if ok in ('y', 'ye', 'yes'):
             return True
         if ok in ('n', 'no', 'nop', 'nope'):
@@ -50,108 +50,55 @@ def ask_ok(prompt, retries=4, complaint='type y/ye/yes or n/no/nop/nope'):
             raise IOError('My give up!')
         print(complaint)
 
-# https://stackoverflow.com/questions/12332975/installing-python-module-within-code#15950647
-def import_or_ask_to_install(package):
-    import importlib
-    try:
-        globals()[package] = importlib.import_module(package)
-    except ImportError:
-        if not ask_ok("install python package |" + package + "|? (y/n): "):
-            return False
-        import pip
-        pip.main(["install", package])
-        globals()[package] = importlib.import_module(package)
-    return True
-
 def check_platform(nameRequired):
     namePlatform = platform.system()
-    print("...running on platform " + namePlatform)
+    print('...running on platform ' + namePlatform)
     if namePlatform != nameRequired:
-        abort("only supported on " + nameRequired)
+        abort('only supported on ' + nameRequired)
 
-def run_shell_command(command, abort_on_error=True):
-    print("`" + command + "`")
-    result = subprocess.run(command, capture_output=True, shell=True, text=True)
+def run_shell_command(command, capture=True, abort_on_error=True):
+    print('`' + command + '`')
+    result = subprocess.run(command, capture_output=capture, shell=True, text=True)
     if abort_on_error and result.returncode:
         abort(result.stderr)
     return result
 
-def setupHomebrew():
-    print("...setting up homebrew...")
-    try:
-        showBrewConfig()
-    except OSError:
-        installHomebrew()
-        showBrewConfig()
-    updateHomebrew()
-
-def setupAnsible():
-    listoutput = listAnsible()
-    if re.search("ansible", listoutput) is None:
-        installAnsible()
-        listoutput = listAnsible()
-    if re.search("ansible 2.8", listoutput) is None:
-        upgradeAnsible(False)
-
-def listAnsible():
-    print("......checking ansible versions...")
-    listoutput = subprocess.check_output("brew list ansible --versions; exit 0", shell=True)
-    print(listoutput)
-    return listoutput
-
-def installAnsible():
-    print("......installing ansible...")
-    print("---------------------------")
-    result = subprocess.call("brew install ansible@2.8", shell=True)
-    print("---------------------------")
-    if result:
-        abort("failed to install ansible")
-
-def upgradeAnsible(retrying):
-    print("......upgrading ansible...")
-    upoutput = subprocess.check_output("brew upgrade ansible@2.8; exit 0",
-        stderr=subprocess.STDOUT, shell=True)
-    print(upoutput)
-    if not retrying and re.search("Xcode alone is not sufficient", upoutput) is not None:
-        installXcodeSelect()
-        upgradeAnsible(True)
-
-def setup_partitions():
-    print("...drive partitions...")
-    print("---------------------------")
+def establish_partitions():
+    print('...drive partitions...')
+    print('---------------------------')
     wiped = False
-    devtarget = "/dev/sda"
-    partboot = devtarget + "1"
-    partroot = devtarget + "2"
-    sizeboot = "256MiB"
-    result = run_shell_command("parted " + devtarget + " print")
-    if result.stdout.find("Partition Table: gpt") < 0:
-        if not wiped and not ask_ok("WIPE and re-partition " + devtarget + " to GPT"):
+    devtarget = '/dev/sda'
+    partboot = devtarget + '1'
+    partroot = devtarget + '2'
+    sizeboot = '256MiB'
+    result = run_shell_command('parted ' + devtarget + ' print')
+    if result.stdout.find('Partition Table: gpt') < 0:
+        if not wiped and not ask_ok('WIPE and re-partition ' + devtarget + ' to GPT'):
             cancel()
-        run_shell_command("parted --script " + devtarget + " mklabel gpt")
+        run_shell_command('parted --script ' + devtarget + ' mklabel gpt')
         wiped = True
-    if result.stdout.find("genboot") < 0:
-        if not wiped and not ask_ok("CREATE boot partition " + partboot):
+    if result.stdout.find('genboot') < 0:
+        if not wiped and not ask_ok('CREATE boot partition ' + partboot):
             cancel()
-        run_shell_command("parted --script " + devtarget +
-                          " mkpart primary 2048s " + sizeboot)
-        run_shell_command("parted --script " + devtarget +
+        run_shell_command('parted --script ' + devtarget +
+                          ' mkpart primary 2048s ' + sizeboot)
+        run_shell_command('parted --script ' + devtarget +
                           " name 1 'genboot'")
-        run_shell_command("parted --script " + devtarget +
-                          " set 1 boot on")
-        run_shell_command("mkfs.ext4 -L GENBOOT " + partboot)
-    if result.stdout.find("genroot") < 0:
-        if not wiped and not ask_ok("CREATE root partition " + partroot):
+        run_shell_command('parted --script ' + devtarget +
+                          ' set 1 boot on')
+        run_shell_command('mkfs.ext4 -L GENBOOT ' + partboot)
+    if result.stdout.find('genroot') < 0:
+        if not wiped and not ask_ok('CREATE root partition ' + partroot):
             cancel()
-        run_shell_command("parted --script " + devtarget +
-                          " mkpart primary " + sizeboot + " 100%")
-        run_shell_command("parted --script " + devtarget +
+        run_shell_command('parted --script ' + devtarget +
+                          ' mkpart primary ' + sizeboot + ' 100%')
+        run_shell_command('parted --script ' + devtarget +
                           " name 2 'genroot'")
-        run_shell_command("mkfs.ext4 -L GENROOT " + partroot)
-        result = run_shell_command("parted " + devtarget + " print")
-    result = run_shell_command("parted " + devtarget + " print")
+        run_shell_command('mkfs.ext4 -L GENROOT ' + partroot)
+        result = run_shell_command('parted ' + devtarget + ' print')
+    result = run_shell_command('parted ' + devtarget + ' print')
     print(result.stdout)
-    print("---------------------------")
+    print('---------------------------')
     return (partboot,partroot)
 
 def first_file_found_matching(filepath, filename):
@@ -159,49 +106,59 @@ def first_file_found_matching(filepath, filename):
         for entry in it:
             if entry.is_file() and re.fullmatch(filename, entry.name):
                 return entry.name
-    return ""
+    return ''
 
 def abspath_of_ancestor_dir(name):
-    apath = os.path.abspath(".")
+    apath = os.path.abspath('.')
     while not not apath and os.path.basename(apath) != name:
         apath = os.path.dirname(apath)
     return apath
 
 def acquire_stage3():
-    print("...acquiring stage3...")
-    print("---------------------------")
-    filepath = os.path.dirname(abspath_of_ancestor_dir("v")) + "/u/soft/open/gentoo"
-    filename = "stage3-amd64-20??????T??????Z.tar.xz"
-    filere   = "^stage3-amd64-20......T......Z\.tar\.xz$"
+    print('...acquiring stage3...')
+    print('---------------------------')
+    filepath = os.path.dirname(abspath_of_ancestor_dir('v')) + '/u/soft/open/gentoo'
+    filename = 'stage3-amd64-20??????T??????Z.tar.xz'
+    filere   = '^stage3-amd64-20......T......Z\.tar\.xz$'
     filespec = filepath + filename
     os.makedirs(filepath, mode=755, exist_ok=True)
     filenamefinal = first_file_found_matching(filepath, filere)
-    if filenamefinal == "":
-        site = "ftp://ftp.rnl.tecnico.ulisboa.pt/pub/gentoo/gentoo-distfiles"
-        url = site + "/releases/amd64/autobuilds/current-stage3-amd64/" + filename
-        run_shell_command("wget -P " + filepath + " " + url)
+    if filenamefinal == '':
+        site = 'ftp://ftp.rnl.tecnico.ulisboa.pt/pub/gentoo/gentoo-distfiles'
+        url = site + '/releases/amd64/autobuilds/current-stage3-amd64/' + filename
+        run_shell_command('wget -P ' + filepath + ' ' + url)
         filenamefinal = first_file_found_matching(filepath, filere)
-        if filenamefinal == "":
-            abort("failed to find or download " + filename)
-    print("---------------------------")
+        if filenamefinal == '':
+            abort('failed to find or download ' + filename)
+    print('---------------------------')
     return os.path.join(filepath, filenamefinal)
 
-def extract_stage3(partroot, tarspec):
-    print("...extracting stage3...")
-    print("---------------------------")
-    run_shell_command("umount -q /mnt/gentoo", abort_on_error=False)
-    run_shell_command("mount " + partroot + " /mnt/gentoo")
-    os.chdir("/mnt/gentoo")
-    run_shell_command("tar -xpvf " + tarspec +
-                      " --xattrs-include='*.*' --numeric-owner")
-    print("---------------------------")
+def establish_stage3(partroot):
+    print('...establishing stage3...')
+    print('---------------------------')
+    pathmount = '/mnt/gentoo'
+    run_shell_command('umount ' + pathmount, abort_on_error=False)
+    run_shell_command('mount ' + partroot + ' ' + pathmount)
+    setdiff = (set(
+        ['bin', 'boot', 'dev', 'etc', 'home', 'lib', 'lib64',
+        'lost+found', 'media', 'mnt', 'opt', 'proc', 'root',
+        'run', 'sbin', 'sys', 'tmp', 'usr', 'var'])
+        - set(sorted(os.listdir(pathmount))))
+    if (setdiff != set()): 
+        print('......missing ' + pathmount + ' subdirectories: ' + str(setdiff))
+        print('......extracting stage3...')
+        run_shell_command('tar -xpvf ' + acquire_stage3() +
+            " --xattrs-include='*.*' --numeric-owner -C, --directory=" + pathmount,
+            capture=False)
+    run_shell_command('ls -alF ' + pathmount, capture=False)
+    print('---------------------------')
 
 def main():
-    print("Setup Gentoo installation...")
-    check_platform("Linux")
-    (partboot, partroot) = setup_partitions()
-    extract_stage3(partroot, acquire_stage3())
-    print("...completed setup of Gentoo installation.")
+    print('Setup Gentoo installation...')
+    check_platform('Linux')
+    (partboot, partroot) = establish_partitions()
+    establish_stage3(partroot)
+    print('...completed setup of Gentoo installation.')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
