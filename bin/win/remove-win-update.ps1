0c4b5bbc49995690b8b6fe84f52bc8f0bcf35f7f
Christopher Augustus <repos@c4augustus.com>
2022-01-02 12:39:59 +0000

    create bin/win/remove-win-update.ps1 to remove Windows Update capability entirely

---
+++ bin/win/remove-win-update.ps1
@@ -0,0 +1,43 @@
+﻿# remove Windows Update capability entirely
+# by moving critical files to C:\Windoze\System32
+#
+$username = (Get-WMIObject -ClassName Win32_ComputerSystem).Username
+$aclperm = $username, "FullControl", "Allow"
+$aclrule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $aclperm
+$dirfrom = "C:\Windows\System32"
+$dirto = "C:\Windoze\System32"
+if (-not (Test-Path $dirto)) {
+    mkdir -p $dirto
+}
+$files = @(
+    "usoapi.dll",
+    "UsoClient.exe",
+    "usocoreps.dll",
+    "usocoreworker.exe",
+    "usosvc.dll",
+    "WaaSAssessment.dll",
+    "WaaSMedicAgent.exe",
+    "WaaSMedicCapsule.dll",
+    "WaaSMedicPS.dll",
+    "WaaSMedicSvc.dll",
+    "wuapi.dll",
+    "wuapihost.exe",
+    "wuauclt.exe",
+    "wuaueng.dll",
+    "wudriver.dll",
+    "wups.dll",
+    "wups2.dll",
+    "wusa.exe",
+    "wuuhext.dll",
+    "wuuhosdeployment.dll"
+)
+foreach ($file in $files) {
+    $filefrom = "$dirfrom\$file"
+    if (Test-Path $filefrom) {
+        takeown /F $filefrom
+        $aclobj = Get-Acl $filefrom
+        $aclobj.SetAccessRule($aclrule)
+        Set-Acl -Path $filefrom -AclObject $aclobj
+        mv $filefrom $dirto\$file
+    }
+}
